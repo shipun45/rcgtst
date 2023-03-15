f999ddeb3d25a3bf903a829704e044b89e909952
shipun45 <hellomayya1@gmail.com>
2023-03-15 14:16:13 +0530

    check

---
+++ routes/plans.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const userRouter = express.Router();
+const auth = require("../middlewares/auth");
+const PlansOrder = require("../models/plansorder");
+const { Plans } = require("../models/plan");
+const User = require("../models/user");
+
+// order product
+userRouter.post("/api/planorder", auth, async (req, res) => {
+    try {
+      const { _id, price, number } = req.body;
+      let plans = [];
+  
+    //   for (let i = 0; i < cart.length; i++) {
+    //     let product = await Product.findById(cart[i].product._id);
+    //     if (product.quantity >= cart[i].quantity) {
+    //       product.quantity -= cart[i].quantity;
+    //       products.push({ product, quantity: cart[i].quantity });
+    //       await product.save();
+    //     } else {
+    //       return res
+    //         .status(400)
+    //         .json({ msg: `${product.name} is out of stock!` });
+    //     }
+    //   }
+  
+      let user = await User.findById(req.user);
+      user.transaction = [];
+      user = await user.save();
+  
+      let order = new Order({
+        plans,
+        price,
+        number,
+        userId: req.user,
+        orderedAt: new Date().getTime(),
+      });
+      order = await order.save();
+      res.json(order);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
+  
