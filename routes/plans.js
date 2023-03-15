0c24c4d6e669c30e45f71bc2b84e2ce5c474e899
shipun45 <hellomayya1@gmail.com>
2023-03-15 14:41:40 +0530

    jhbhjbh

--- routes/plans.js
+++ routes/plans.js
@@ -12,31 +12,13 @@ planRouter.post("/api/planorder", auth, async (req, res) => {
     const  data = req.body;
       let plans = [];
   
-    //   for (let i = 0; i < cart.length; i++) {
-    //     let product = await Product.findById(cart[i].product._id);
-    //     if (product.quantity >= cart[i].quantity) {
-    //       product.quantity -= cart[i].quantity;
-    //       products.push({ product, quantity: cart[i].quantity });
-    //       await product.save();
-    //     } else {
-    //       return res
-    //         .status(400)
-    //         .json({ msg: `${product.name} is out of stock!` });
-    //     }
-    //   }
   
       let user = await User.findById(req.user);
       user.transaction = [];
       user = await user.save();
   
-    //   let order = new Order({
-    //     plans,
-    //     price,
-    //     number,
-    //     userId: req.user,
-    //     orderedAt: new Date().getTime(),
-    //   });
-      plans = await data.save();
+   
+    //   plans = await data.save();
       res.json(user);
     } catch (e) {
       res.status(500).json({ error: e.message });
