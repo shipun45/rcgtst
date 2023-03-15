a03bdfc21773d6e838885e5210ac46a89c7c6acd
shipun45 <hellomayya1@gmail.com>
2023-03-15 13:02:12 +0530

    plans models added

---
+++ models/plan.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+
+const planSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  ratings: [ratingSchema],
+});
+
+const Plans = mongoose.model("Plans", planSchema);
+module.exports = { Plans, planSchema };
